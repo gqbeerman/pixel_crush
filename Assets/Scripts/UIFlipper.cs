42d13a34a1af397a19da18aa42448660a5e98528
Alex Miller <lxmllr71@gmail.com>
2019-02-26 16:08:24 -0800

    add UIFipper to enable objects for different modes

---
+++ Assets/Scripts/UIFlipper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFlipper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer timer = GetComponentInChildren<Timer>(true);
+        CollectionPanelSpawner collection = GetComponentInChildren<CollectionPanelSpawner>(true);
+        timer.gameObject.SetActive(false);
+        collection.gameObject.SetActive(false);
+
+        LevelGoal goal = GameManager.Instance.GetComponent<LevelGoal>();
+        if (goal is LevelGoalCollected) {
+            collection.gameObject.SetActive(true);
+        }
+        else if (goal is LevelGoalTimed) {
+            timer.gameObject.SetActive(true);
+        }
+        
+    }
+}
